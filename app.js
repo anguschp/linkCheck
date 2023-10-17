528479e2738740f5098b2fb1f6e34ff0f4a965e4
Angus <anguschp@gmail.com>
2023-10-17 13:28:53 +0800

    get folder files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const xml2js = require('xml2js');
 const axios = require('axios');
+const fs = require("fs");
 const { parse } = require('path');
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.text({ type: 'text/xml' }));
 app.post('/upload', async(req, res) => {
   
 var url = req.query.url;
-console.log("Check URL: " + url);
+var folderPath = req.query.folderPath;
 
   if (!url) {
     return res.status(400).send('Missing URL parameter');
@@ -20,32 +21,32 @@ console.log("Check URL: " + url);
   
 
   try {
-    // Fetch the XML file from the provided URL
     const response = await axios.get(url, {
       responseType: 'text'
     });
 
     const xmlData = response.data;
 
-    // Parse the XML data
     xml2js.parseString(xmlData, (err, result) => {
+      
       if (err) {
         console.error('Error parsing XML:', err);
         return res.status(400).send('Invalid XML data');
       }
-
-      // Extract the necessary data from the parsed result
       const dataArray = extractData(result);
-
-      // Send a response back to the client
-      //Angus Comments
+      processDownloadedfiles(folderPath);
+    
       res.status(200).send(JSON.stringify(dataArray));
+
     });
   } catch (error) {
+
     console.error('Error fetching XML file:', error);
     res.status(400).send('Failed to fetch XML file');
+
   }
 });
+
 
 // Helper function to extract data from the parsed XML result
 function extractData(parsedXml) {
@@ -74,6 +75,16 @@ function extractData(parsedXml) {
     console.log(tempArray[1]);
     return tempArray;
 }
+
+function processDownloadedfiles(folderPath){
+
+  fs.readdir(folderPath, (err, files) =>{
+    console.log(files.length);
+    console.log(files);
+  })
+
+}
+
 
 
 // Start the server
